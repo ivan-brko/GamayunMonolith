493dac118adcc975d686a75d3d6145d6153c9084
Ivan Brko <ivan.brko@protonmail.com>
2020-01-03 17:03:36 +0100

    Added grpc to main project

--- GamayunMonolith/Program.fs
+++ GamayunMonolith/Program.fs
@@ -7,4 +7,8 @@ let main argv =
     let config =
         ConfigurationReader.Toml.ReadConfigurationsFromPath
             "/home/ivan/Projects/fsharp/GamayunMonolith/ConfigurationReader/SampleConfigurations"
+
+    let grpcPort = 50052
+    use server = new Gamayun.GrpcServer(grpcPort, fun x -> printfn "Got a message from id %d" x.JobId)
+            
     0 // return an integer exit code
